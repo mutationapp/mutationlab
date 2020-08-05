e2666c274d8c9a0d357e02d634ea46e6fe871136
ibrahim velinov <ibsukru@gmail.com>
2020-08-05 21:40:09 +0200

    🚀

--- packages/mutationlab.101/src/math/math.ts
+++ packages/mutationlab.101/src/math/math.ts
@@ -9,7 +9,8 @@ const multiply = (x: number, y: number) => x * y
 /**
   This will have 100% code coverage but mutation covarage is 0%,
   just because tests doesnt provide correct samples,
-  they are but not good enough to prove the funtionality
+  they are but not good enough to prove the funtionality,
+  see the solution here https://github.com/mutationapp/mutationlab/blob/101/packages/mutationlab.101/src/math/math.test.ts
  */
 
 export { add, subtract, divide, multiply }
