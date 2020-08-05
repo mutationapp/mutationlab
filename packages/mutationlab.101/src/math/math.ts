c649ccac2020c420658874bb860259b7525c5a4d
ibrahim velinov <ibsukru@gmail.com>
2020-08-05 22:00:02 +0200

    🚀

--- packages/mutationlab.101/src/math/math.ts
+++ packages/mutationlab.101/src/math/math.ts
@@ -1,3 +1,10 @@
+/**
+  This file will have 100% code coverage but mutation coverage is 0%, 
+  just because tests doesnt provide correct samples, 
+  and not good enough to prove the functionality,
+  see the solution here https://github.com/mutationapp/mutationlab/blob/101/packages/mutationlab.101/src/math/math.test.ts
+ */
+
 const add = (x: number, y: number) => x + y
 
 const subtract = (x: number, y: number) => x - y
@@ -5,12 +12,5 @@ const subtract = (x: number, y: number) => x - y
 const divide = (x: number, y: number) => x / y
 
 const multiply = (x: number, y: number) => x * y
-
-/**
-  This will have 100% code coverage but mutation coverage is 0%, 
-  just because tests doesnt provide correct samples, 
-  and not good enough to prove the functionality,
-  see the solution here https://github.com/mutationapp/mutationlab/blob/101/packages/mutationlab.101/src/math/math.test.ts
- */
 
 export { add, subtract, divide, multiply }
