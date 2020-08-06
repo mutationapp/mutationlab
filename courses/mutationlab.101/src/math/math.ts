33b39cebd3b224cf08df73095916428141539e1a
ibrahim velinov <ibsukru@gmail.com>
2020-08-06 19:07:11 +0200

    🚀

--- courses/mutationlab.101/src/math/math.ts
+++ courses/mutationlab.101/src/math/math.ts
@@ -3,7 +3,7 @@
   This module has 100% code coverage but mutation coverage is 50%, 
   just because tests doesn't provide correct context, 
   and not good enough to prove the functionality,
-  see solution here https://github.com/mutationapp/mutationlab/blob/101/courses/mutationlab.101/src/math/math.test.ts
+  see the solution here https://github.com/mutationapp/mutationlab/blob/101/courses/mutationlab.101/src/math/math.test.ts
  */
 
 const add = (x: number, y: number) => x + y
