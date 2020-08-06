a336ea28bb301a6168e70655dff200f1cd03981c
ibrahim velinov <ibsukru@gmail.com>
2020-08-06 19:02:30 +0200

    🚀

--- courses/mutationlab.101/src/math/math.ts
+++ courses/mutationlab.101/src/math/math.ts
@@ -2,10 +2,8 @@
   This file will have 100% code coverage but mutation coverage is 50%, 
   just because tests doesnt provide correct samples, 
   and not good enough to prove the functionality,
-  see the solution here https://github.com/mutationapp/mutationlab/blob/101/courses/mutationlab.101/src/math/math.test.ts
+  solution here https://github.com/mutationapp/mutationlab/blob/101/courses/mutationlab.101/src/math/math.test.ts
  */
-
-// const add = () => undefined
 
 const add = (x: number, y: number) => x + y
 
